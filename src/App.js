bdbdf8d7ae44dc1ca2408ce8a398bcb0dc189ebe
Anhtuanzzz <62417928+Anhtuanzzz@users.noreply.github.com>
2022-03-24 20:36:38 +0700

    more update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Home from "./components/Home";
 import GraduationPoint from "./components/GraduationPoint";
+import EntrancePoint from "./components/EntrancePoint";
+import About from './components/About';
 import {
   BrowserRouter,
   Routes,
@@ -16,13 +18,15 @@ function App() {
           <ul>
             <li><Link to='/'>Trang chủ</Link></li>
             <li><Link to = '/totnghiep'>Tính điểm tốt nghiệp</Link></li>
-            <li>Tính điểm theo khối thi</li>
-            <li>About</li>
+            <li><Link to = '/daihoc'>Tính điểm theo khối thi</Link></li>
+            <li><Link to = '/about'>About</Link></li>
           </ul>
         </nav>
         <Routes>
           <Route path = '/' element = {<Home />} />
           <Route path = '/totnghiep' element = {< GraduationPoint />} />
+          <Route path = '/daihoc' element = {<EntrancePoint />} />
+          <Route path = '/about' element = {<About />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <div>
+            <h1>About me:</h1>
+            <p>Nguyen Anh Tuan</p>
+            <a href="https://anhtuanzzz.github.io" target="blank">Portfolio</a>
+        </div>
+    );
+}
